06859ed43ae43a4ccb48cdb3d239a7325003e94b
vishal-virani <viranivishal123@gmail.com>
2019-07-25 22:59:02 +0530

    add android

---
+++ android/app/src/main/java/com/sagasapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.sagasapp;
+
+import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "SagasApp";
+    }
+     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, true);  // here
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ android/app/src/main/java/com/sagasapp/MainApplication.java
@@ -0,0 +1,57 @@
+package com.sagasapp;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNGestureHandlerPackage(),
+            new LinearGradientPackage(),
+            new VectorIconsPackage(),
+            new RNDeviceInfo(),
+            new SplashScreenReactPackage(),
+            new AsyncStoragePackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
